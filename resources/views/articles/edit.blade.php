bc0eed1154dc6e8855978c2a3c4612b4a3203f08
georgeadeishvili <giorgi.adeishvili@gmail.com>
2021-05-27 00:16:32 +0400

    initial commit

---
+++ resources/views/articles/edit.blade.php
@@ -0,0 +1,3 @@
+<x-layout>
+	<x-slot name='title'>Index</x-slot>
+</x-layout>
---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>{{ $title ?? 'Articles' }}</title>
+</head>
+
+<body>
+	{{ $child }}
+</body>
+
+</html>
